98f199dbd4663fde72f5f90d190d6495cbfb3692
alvalenda <banysan@msn.com>
2022-08-13 07:31:59 -0300

    +startGame +resetGame +displayPokemon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
 const pokeAPIBaseUrl = "https://pokeapi.co/api/v2/";
+const pokemonContainer = document.querySelector("#pokemon-container");
+const resetButton = document.querySelector("#reset-button");
 
 const loadPokemon = async (pokemonNumber) => {
   const response = fetch(`${pokeAPIBaseUrl}pokemon/${pokemonNumber}`);
@@ -15,23 +17,43 @@ const randomPokemon = async () => {
 
 const buildPokemonArray = async () => {
   const randomIds = [...(await randomPokemon())];
-  console.log(randomIds);
   const promises = randomIds.map((id) => loadPokemon(id));
   const responses = await Promise.all(promises);
   const data = await Promise.all(responses.map((response) => response.json()));
   return data;
 };
 
-const displayPokemon = (pokeArray) => {};
+const displayPokemon = (pokepairs) => {
+  pokepairs.sort((_) => Math.random() - 0.5);
+  pokepairs.map((p) => console.log(p.name));
+
+  pokepairs.forEach((pokemon) => {
+    const { name, sprites } = pokemon;
+    const { front_default } = sprites;
+    const div = document.createElement("div", { className: "card" });
+    const img = document.createElement("img");
+    img.src = front_default;
+    img.alt = name;
+    div.appendChild(img);
+    pokemonContainer.appendChild(div);
+  });
+};
 
 const resetGame = async (pokeArray) => {
   pokeArray = await buildPokemonArray();
-  displayPokemon(pokeArray);
+  pokemonContainer.textContent = "";
+  displayPokemon([...pokeArray, ...pokeArray]);
+};
+
+const startGame = async (pokeArray) => {
+  displayPokemon([...pokeArray, ...pokeArray]);
+  resetButton.addEventListener("click", async () => {
+    resetGame();
+  });
 };
 
 const main = async () => {
   const pokeArray = await buildPokemonArray();
-  console.log(pokeArray);
+  startGame(pokeArray);
 };
-
 main();
